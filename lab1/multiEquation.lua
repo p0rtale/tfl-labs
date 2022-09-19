1eb60f96e34b3fc45ce579c07eaff66aaec30983
p0rtale <petryaev_1984@mail.ru>
2022-09-19 14:35:18 +0300

    lab1

---
+++ lab1/multiEquation.lua
@@ -0,0 +1,166 @@
+local utilsMod = require("utils")
+local termMod = require("term")
+
+local multieqMod = {}
+
+multieqMod.MultiEquations = {}
+
+function multieqMod.MultiEquations:new()
+    local public = {}
+    local private = {}
+
+    private.arr = {}
+
+    function public:getArr()
+        return private.arr
+    end
+
+    function public:compactAppend(multiEq)
+        local var = multiEq.getVariables()[1]
+        for i = 1, #private.arr do
+            local curMultiEq = private.arr[i]
+            local curVars = curMultiEq.getVariables()
+            local curTerms = curMultiEq.getTerms()
+            if utilsMod.containsData(curVars, var) then
+                utilsMod.appendValues(curTerms, multiEq.getTerms())
+                return
+            end
+        end
+        utilsMod.appendValue(private.arr, multiEq)
+    end
+
+    self.__index = self
+    return setmetatable(public, self)
+end
+
+
+multieqMod.MultiEquation = {}
+
+function multieqMod.MultiEquation:new(variables, terms)
+    local public = {}
+    local private = {}
+
+    private.variables = variables
+    private.terms = terms
+
+    function public.getVariables()
+        return private.variables
+    end
+
+    function public.getTerms()
+        return private.terms
+    end
+
+    function private:getLevelVariable(nodes)
+        local variable = nil
+        local constructor = nil
+
+        for i = 1, #nodes do
+            if nodes[i]:isVariable() then
+                variable = nodes[i].data
+            elseif nodes[i]:isConstructor() then
+                if constructor ~= nil and constructor ~= nodes[i].data then
+                    error("different constructors")
+                end
+                constructor = nodes[i].data
+            end
+        end
+
+        return variable
+    end
+
+    function private:getCommonPartRec(commonNode, border, roots, idx)
+        local variable = private:getLevelVariable(roots)
+        if variable ~= nil then
+            commonNode.childs[idx] = termMod.Node:new(variable, 0)
+
+            local terms = {}
+            for i = 1, #roots do
+                if roots[i].data ~= variable then
+                    utilsMod.appendValue(terms, roots[i])
+                end
+            end
+            local x = multieqMod.MultiEquation:new({ termMod.Node:new(variable, 0) }, terms)
+            border:compactAppend(x)
+            return
+        end
+
+        local root = roots[1]
+        local constructor = root.data
+        commonNode.childs[idx] = termMod.Node:new(constructor, constructor:getArgNum())
+
+        for i = 1, #root.childs do
+            local childRoots = {}
+            for j = 1, #roots do
+                childRoots[j] = roots[j].childs[i]
+            end
+            private:getCommonPartRec(commonNode.childs[idx], border, childRoots, i)
+        end
+    end
+
+    function public:getCommonPart()
+        if #private.terms == 0 then
+            return nil, nil
+        end
+        if #private.terms == 1 then
+            return private.terms[1], nil
+        end
+
+        local roots = {}
+        for i = 1, #private.terms do
+            roots[i] = private.terms[i]
+        end
+
+        local commonPart
+        local border = multieqMod.MultiEquations:new()
+        local variable = private:getLevelVariable(roots)
+        if variable ~= nil then
+            commonPart = termMod.Node:new(variable, 0)
+
+            local terms = {}
+            for i = 1, #roots do
+                if roots[i].data ~= variable then
+                    utilsMod.appendValue(terms, roots[i])
+                end
+            end
+            local x = multieqMod.MultiEquation:new({ termMod.Node:new(variable, 0) }, terms)
+            border:compactAppend(x)
+
+            return commonPart, border
+        end
+
+        local root = roots[1]
+        commonPart = termMod.Node:new(root.data, #root.childs)
+
+        for i = 1, #root.childs do
+            local childRoots = {}
+            for j = 1, #roots do
+                childRoots[j] = roots[j].childs[i]
+            end
+            private:getCommonPartRec(commonPart, border, childRoots, i)
+        end
+
+        return commonPart, border
+    end
+
+    function public:toStr()
+        local str = "{"
+        local vars = public:getVariables()
+        for i = 1, #vars do
+            str = str .. vars[i]:toStr() .. ","
+        end
+        str = utilsMod.replaceChar(str, #str, "}")
+        str = str .. " = ("
+        local terms = public:getTerms()
+        for i = 1, #terms do
+            str = str .. terms[i]:toStr() .. ","
+        end
+        str = utilsMod.replaceChar(str, #str, ")")
+        return str
+    end
+
+    self.__index = self
+    return setmetatable(public, self)
+end
+
+return multieqMod
---
+++ lab1/term.lua
@@ -0,0 +1,164 @@
+local utilsMod = require("utils")
+
+local termMod = {}
+
+termMod.Constructor = {}
+
+function termMod.Constructor:new(name, argNum)
+    local public = {}
+    local private = {}
+
+    public.name = name
+    public.argNum = argNum
+
+    function public:getName()
+        return public.name
+    end
+
+    function public:getArgNum()
+        return public.argNum
+    end
+
+    self.__index = self
+    return setmetatable(public, self)
+end
+
+function termMod.parseConstructors(str)
+    local constructors = {}
+    local first, last = str:find("=")
+    while true do
+        first, last = str:find("%a%(%d+%)", last + 1)
+        if first == nil then
+            break
+        end
+        local name = str:sub(first, first)
+        local arity = tonumber(str:sub(first + 2, last - 1))
+        constructors[name] = termMod.Constructor:new(name, arity)
+    end
+    return constructors
+end
+
+
+termMod.Variable = {}
+
+function termMod.Variable:new(name)
+    local public = {}
+    local private = {}
+
+    public.name = name
+
+    function public:getName()
+        return public.name
+    end
+
+    function public:getArgNum()
+        return nil
+    end
+
+    self.__index = self
+    return setmetatable(public, self)
+end
+
+function termMod.parseVariables(str)
+    local variables = {}
+    local i = str:find("=")
+    while true do
+        i = str:find("%a", i + 1)
+        if i == nil then
+            break
+        end
+        local name = str:sub(i, i)
+        variables[name] = termMod.Variable:new(name)
+    end
+    return variables
+end
+
+
+termMod.Node = {}
+
+function termMod.Node:new(data, childsNum)
+    local public = {}
+    local private = {}
+
+    local arr = {}
+    for i = 1, childsNum do
+        arr[i] = {}
+    end
+
+    public.childs = arr
+    public.data = data
+
+    function public:isVariable()
+        return public.data.getArgNum() == nil
+    end
+
+    function public:isConstructor()
+        return public.data.getArgNum() >= 0
+    end
+
+    function private:constructorToStr()
+        local str = public.data:getName()
+
+        str = str .. "("
+        for i = 1, #public.childs do
+            str = str..public.childs[i]:toStr()
+            str = str .. ","
+        end
+        str = utilsMod.replaceChar(str, #str, ")")
+
+        return str
+    end
+
+    function public:toStr()
+        if public:isVariable() then
+            return public.data:getName()
+        end
+        return private:constructorToStr()
+    end
+
+    self.__index = self
+    return setmetatable(public, self)
+end
+
+
+function termMod.parseTermRec(str, variables, constructors)
+    local term
+    if #str == 1 then
+        term = termMod.Node:new(variables[str:sub(1, 1)], 0)
+    else
+        local constructor = constructors[str:sub(1, 1)]
+        local argNum = constructor:getArgNum()
+        term = termMod.Node:new(constructor, argNum)
+        local first = 3
+        local last = first + 1
+        for i = 1, argNum do
+            local level = 0
+            if str:sub(last, last) == "(" then
+                level = 1
+                last = last + 1
+            end
+            while level ~= 0 do
+                local c = str:sub(last, last)
+                if c == "(" then
+                    level = level + 1
+                elseif c == ")" then
+                    level = level - 1
+                end
+                last = last + 1
+            end
+            term.childs[i] = termMod.parseTermRec(str:sub(first, last - 1), variables, constructors)
+            if i < argNum then
+                first = str:find("%a", last)
+                last = first + 1
+            end
+        end
+    end
+    return term
+end
+
+function termMod.parseTerm(str, variables, constructors)
+    local _, last = str:find(":%s*%a")
+    return termMod.parseTermRec(str:sub(last, #str), variables, constructors)
+end
+
+return termMod
